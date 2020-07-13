a42028cf3377dc74db6a38df753716372e9c8208
devanimesh1 <68100249+devanimesh1@users.noreply.github.com>
2020-07-14 03:33:19 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+const axios = require('axios');
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+ 
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+ 
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+ 
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+ 
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function weatherWebHandeler (agent){
+    const city = agent.parameters.city;
+    const API_KEY = "enter-your-api-key-from-owm";
+	const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+    
+    return axios.get(url)
+    .then((result) => {
+      console.log(result.data.weather[0].description);
+      agent.add(`The current weather condition in ${city} is: `);
+      agent.add(result.data.weather[0].description+ ` and the temerature is`);
+    }).catch((err)=>{
+      agent.add(`No city ${city} found`);
+    });
+  }
+  
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('weatherTest', weatherWebHandeler);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
